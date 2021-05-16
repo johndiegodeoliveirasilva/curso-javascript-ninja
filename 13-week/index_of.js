824db9ba034ebdd0029bd1a68dcae75cdae4690b
John Diego <johnoliveirasilva9@gmail.com>
2021-05-15 23:55:20 -0300

    IndexOf, lastIndexOf and is array

---
+++ 13-week/index_of.js
@@ -0,0 +1,13 @@
+var arr = [ 1, 2, 3, 4, 5];
+
+console.log( arr.indexOf(3, 2));
+
+var index = arr.lastIndexOf(1)
+console.log(arr.lastIndexOf(1))
+console.log(arr[index])
+
+// Is a Array
+
+var arr = ['D', 'a', 'c', 'i', 'u', 'k']
+
+console.log( Array.isArray(arr))
